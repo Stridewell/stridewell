de921bc3535724888966da17b04d5d91d5ecff33
Kevin Tempels <Kevin.Tempels@esc.be>
2024-09-22 19:46:07 +0200

    cleanup models and migrations

--- apps/backend/stridewell/models.py
+++ apps/backend/stridewell/models.py
@@ -1,9 +1,17 @@
+from django import utils
 from django.db import models
 
 # Create your models here.
 
+class Runner(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    date_of_birth = models.DateField()
+
 class Workout(models.Model):
     name = models.CharField(max_length=200)
+    runner = models.ForeignKey(to=Runner, on_delete=models.CASCADE)
+    date = models.DateTimeField(default=utils.timezone.now)
 
     def __str__(self):
         return self.name
