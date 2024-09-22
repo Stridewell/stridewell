1bbf0e7a3014a319b93f4934fa556d8139f6a7b7
Kevin Tempels <Kevin.Tempels@esc.be>
2024-09-22 18:38:44 +0200

    edit gitignore

--- apps/backend/backend/urls.py
+++ apps/backend/backend/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path("stridewell/", include("stridewell.urls")),
+    path("", include("stridewell.urls")),
     path('admin/', admin.site.urls),
 ]
