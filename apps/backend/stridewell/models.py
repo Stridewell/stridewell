06e98b67c736c8365dbc7d91790b1ad2fc435ee9
Kevin Tempels <Kevin.Tempels@esc.be>
2024-09-23 20:49:06 +0200

    extend models to be usable

--- apps/backend/stridewell/models.py
+++ apps/backend/stridewell/models.py
@@ -1,8 +1,6 @@
 from django import utils
 from django.db import models
 
-# Create your models here.
-
 class Runner(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
@@ -16,9 +14,35 @@ class Workout(models.Model):
     def __str__(self):
         return self.name
     
+class WorkoutSet(models.Model):
+    workout = models.ForeignKey(to=Workout, on_delete=models.CASCADE),
+    order = models.IntegerField(default=1),
+    name = models.CharField(max_length=200),
+    repeat_amount = models.IntegerField(default=1),
+
+    def __str__(self):
+        return self.name
+    
+DURATION_TYPE_CODES = (
+    ('DISTANCE', 'Distance'),
+    ('TIME', 'Time'),
+)
+
+TARGET_TYPE_CODES = (
+    ('SPEED', 'Speed'),
+    ('HEART_RATE', 'Heart rate'),
+    ('CADENCE', 'Cadence'),
+)
+    
 class WorkoutStep(models.Model):
-    workout = models.ForeignKey(to=Workout, on_delete=models.CASCADE)
-    name = models.CharField(max_length=200)
+    workout = models.ForeignKey(to=Workout, on_delete=models.CASCADE),
+    set = models.ForeignKey(to=WorkoutSet, on_delete=models.CASCADE, null=True, blank=True)
+    order = models.IntegerField(default=1),
+    name = models.CharField(max_length=200),
+    duration_type = models.CharField(choices=DURATION_TYPE_CODES),
+    duration_value = models.IntegerField(default=1)
+    target_type = models.CharField(choices=TARGET_TYPE_CODES),
+    target_value = models.IntegerField(default=1)
 
     def __str__(self):
         return self.name
